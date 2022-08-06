5e1e5b79f66a1bf6cb992a38e7e704c705ab1592
Mohd Fidzrin <ext-mohd.fidzrin@solenovo.fi>
2022-08-06 23:52:31 +0800

    refactored to be able to do sort either by ascending or descending order

--- SortSalary.java
+++ SortSalary.java
@@ -1,9 +1,14 @@
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SortSalary {
+
+    private enum Order {
+        ASC, DESC
+    }
 
     public static void main(String[] args) {
         Map<Integer, Integer> unsortedSalary = new HashMap<>();
@@ -12,13 +17,20 @@ public class SortSalary {
         unsortedSalary.put(5, 13000);
         unsortedSalary.put(4, 2000);
         unsortedSalary.put(2, 6000);
-        LinkedHashMap<Object, Object> sort = sortByValueInDescending(unsortedSalary);
+        LinkedHashMap<Object, Object> sort = sortByValue(unsortedSalary, Order.DESC);
         System.out.println(sort);
     }
 
-    private static LinkedHashMap<Object, Object> sortByValueInDescending(Map<Integer, Integer> map) {
+    private static LinkedHashMap<Object, Object> sortByValue(Map<Integer, Integer> map, Order order) {
         return map.entrySet().stream()
-                .sorted((o1, o2) -> o2.getValue().compareTo(o1.getValue()))
+                .sorted(getComparator(order))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (x, y) -> y, LinkedHashMap::new));
+    }
+
+    private static Comparator<Map.Entry<Integer, Integer>> getComparator(Order order) {
+        if (order.equals(Order.DESC)) {
+            return (o1, o2) -> o2.getValue().compareTo(o1.getValue());
+        }
+        return Map.Entry.comparingByValue();
     }
 }
